1176d8c4d880052373cc6fd5975bca9b38e8df48
MuskanKaushik <muskankaushik0872@gmail.com>
2021-07-02 21:32:31 +0530

    commit changes

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+//This file is going to take all the components and going to export it to the App.js file -->>
+
+export {default as Cards} from './Cards/Cards';
+export {default as Chart} from './Chart/Chart';
+export {default as CountryPicker} from './CountryPicker/CountryPicker';
+
